a9b2ccaf8012b120501467214be5ac98fddafcb9
Luis Bryan Cueva <lcueva@tesabiz.com>
2023-11-28 18:36:15 -0400

    fix

--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -332,7 +332,7 @@
         <footer class="main-footer">
             <!-- To the right -->
             <div class="float-right d-none d-sm-inline">
-                Parcial I - SW
+                Parcial II - SW
             </div>
             <!-- Default to the left -->
             <strong>Copyright &copy; 2022 <a href="https://adminlte.io">Luis B</a>.</strong> All rights
